fbdf29efdad8df104103c5153248083ea6e7f607
Danila <zaharchuk.danila@18gmail.com>
2026-01-09 21:45:24 +0300

    init: 5-oop

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 "use strict";
+
+class Person {
+  constructor(rase, name, language) {
+    this.rase = rase;
+    this.name = name;
+    this.language = language;
+  }
+
+  getGreetings() {
+    console.log(`Язык: ${this.language}, Имя: ${this.name}`);
+  }
+}
+
+class Orc extends Person {
+  constructor(rase, name, language, weapon) {
+    super(rase, name, language);
+    this.weapon = weapon;
+  }
+  getHits() {
+    console.log(`Метод удара, оружие: ${this.weapon}`);
+  }
+}
+
+class Elfi extends Person {
+  constructor(rase, name, language, typeOfMagic) {
+    super(rase, name, language);
+    this.typeOfMagic = typeOfMagic;
+  }
+  getCreateMagic() {
+    console.log(`Создать заклинание благодаря: ${this.typeOfMagic}`);
+  }
+}
+
+const orc = new Orc("Чернокнижник", "Alan", "English", "Hammer");
+orc.getGreetings();
+orc.getHits();
+
+const elf = new Elfi("God", "Alina", "Russian", "Repear");
+elf.getGreetings();
+elf.getCreateMagic();
