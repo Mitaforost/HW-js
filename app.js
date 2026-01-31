fe0ea27ebf8605929bf50a9d8dc7afef53a2cf7e
Danila <zaharchuk.danila@18gmail.com>
2026-01-31 11:26:59 +0300

    init: 11-geo

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 "use strict";
+
+function getGeolocation() {
+  return new Promise((resolve, reject) => {
+    if (!navigator.geolocation) {
+      reject(new Error("Geolocation не поддерживается"));
+      return;
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+      resolve({
+        latitude: position.latitude,
+        longitude: position.longitude,
+      });
+      (error) => {
+        reject(error);
+      };
+    });
+  });
+}
+
+getGeolocation()
+  .then((coords) => {
+    console.log(coords.latitude, coords.longitude);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
