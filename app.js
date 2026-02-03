becc1ddbe98ff7a542f67057797d0da9937570a3
Danila <zaharchuk.danila@18gmail.com>
2026-02-03 19:51:13 +0300

    init: 12

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 "use strict";
+
+function race(promises) {
+  return new Promise((resolve, reject) => {
+    for (const promise of promises) {
+      Promise.resolve(promise).then(resolve).catch(reject);
+    }
+  });
+}
+
+const one = new Promise((resolve) => setTimeout(() => resolve("one"), 300));
+
+const two = new Promise((resolve) => setTimeout(() => resolve("two"), 100));
+
+const three = new Promise((_, reject) =>
+  setTimeout(() => reject("error"), 200),
+);
+
+race([one, two, three]).then(console.log).catch(console.error);
